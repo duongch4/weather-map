12cccfdb65f15e22dd09a34967fe0ed5e5d8ccb1
Bang Chi Duong <chibangduong1993@gmail.com>
2020-04-15 12:46:34 -0700

    Added an option for infinite loading bar

--- client/components/progressbar/ProgressBar.tsx
+++ client/components/progressbar/ProgressBar.tsx
@@ -3,6 +3,7 @@ import React from "react";
 type ProgressBarProps = {
     progress: number;
     hide: boolean;
+    indeterminate?: boolean;
 };
 
 export const ProgressBar = (props: ProgressBarProps) => {
@@ -11,13 +12,21 @@ export const ProgressBar = (props: ProgressBarProps) => {
         visibility: (props.hide) ? "hidden" : "visible" as "visible" | "hidden"
     };
 
-    const progressBarStyle = {
-        width: `${props.progress}%`,
+    const progressBarWidth = (barWidth: number) => {
+        return {
+            width: `${barWidth}%`
+        };
     };
 
     return (
         <div className="progress fixed-top" style={progressStyle}>
-            <div className="progress-bar bg-info progress-bar-striped" style={progressBarStyle}>{props.progress}</div>
+            <div
+                className="progress-bar bg-info progress-bar-striped progress-bar-animated"
+                style={(props.indeterminate) ? progressBarWidth(100) : progressBarWidth(props.progress)}
+                area-valuenow={(props.indeterminate) ? 100 : props.progress}
+                area-valuemin={0}
+                area-valuemax={100}
+            >{(props.indeterminate) ? undefined : props.progress}</div>
         </div>
     );
 };
